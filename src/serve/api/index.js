0c392fcde4c13b9e8816ca62a4de62fe61432b94
ShuangMuSauce <332276093@qq.com>
2020-11-03 14:53:49 +0800

    封装axios网络请求

---
+++ src/serve/api/index.js
@@ -0,0 +1,5 @@
+import ajax from './ajax'
+
+//1.定义基础路径
+const BASE_URL= 'http://demo.itlike.com/web/xlmc';
+export const getHomeData=()=>ajax(BASE_URL+'/api/homeApi');
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,8 +5,17 @@
 </template>
 
 <script type="text/javascript">
+
+import{ getHomeData } from './../../serve/api/index.js'
 export default {
 name: "Home",
+  created () {
+    getHomeData().then(response=>{
+      console.log(response);
+    }).catch(error=>{
+      console.log(error);
+    });
+  },
   data() {
   return {
 
